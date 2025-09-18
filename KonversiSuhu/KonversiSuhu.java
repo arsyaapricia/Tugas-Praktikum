de01cb017bcc38472300d19ace7b2a1bb79d778c
Arsya Apricia Purnomo <arsyaapriciap@gmail.com>
2025-09-18 11:25:56 +0700

    Add files via upload

---
+++ KonversiSuhu/KonversiSuhu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package KonversiSuhu;
+
+/**
+ *
+ * @author User
+ */
+public class KonversiSuhu {
+    
+    public int celciusToFahrenheit(int c) {
+        return (int)(c * 9 / 5) + 32;
+    }
+    public int celciusToReamur(int c) {
+        return (int) (c * 4 / 5);
+    }
+}
+    
